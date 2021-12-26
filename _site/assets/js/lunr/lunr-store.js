4c2440306027964c62d2a90bbfb1ce83330c17c2
Votamrima <Votamrima@email>
2021-12-26 14:31:57 +0100

    just another commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Sample post",
+        "excerpt":"   Most used regex paterns   Other patterns   Examples:   Source   This is a sample post  ","categories": [],
+        "tags": ["test"],
+        "url": "/languages/test-test/",
+        "teaser": null
+      }]
